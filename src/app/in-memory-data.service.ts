e7ea3cd08e92b47edc5008694f423ceee7f2ec64
GEAY <cecile.geay.externe@sogeti.com>
2019-07-10 17:02:35 +0200

    ajout d'un héros

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -11,6 +11,7 @@ export class InMemoryDataService {
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
+	  { id: 21, name: 'CheshireCat' }
     ];
     return { heroes };
   }
